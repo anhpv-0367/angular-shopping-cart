1fd5e6fc3074ce902d82ae3b33da16a9403dcfa1
anhpv-0367 <pham.viet.anh@sun-asterisk.com>
2020-04-26 16:55:26 +0700

    mock data in memory web api

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,13 +1,18 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Product } from '../shared/model/product.model'
 
 @Injectable()
+
 export class ProductService {
+  private productsUrl = 'api/products';  // URL to web api
+
   constructor(
     private http: HttpClient
   ) { }
-
-  getProducts() {
-    return this.http.get('/assets/products.json');
+  
+  getProducts(): Observable<Product[]> {
+    return this.http.get<Product[]>(this.productsUrl)
   }
 }
---
+++ src/app/shared/model/product.model.ts
@@ -0,0 +1,13 @@
+export class Product {
+  id: number;
+  sku: string;
+  title: string;
+  description: string;
+  availableSizes: any;
+  style: string;
+  price: number;
+  installments: number;
+  currencyId: string;
+  currencyFormat: string;
+  isFreeShipping: boolean
+}
